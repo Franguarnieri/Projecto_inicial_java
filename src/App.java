2621783890e1f3a167dc91fc0fd6c573ea96dd7e
Franco Guarnieri <francoguarnieri90@gmail.com>
2025-03-11 19:01:36 -0300

    modifciadosa en impresión

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("4to comiteo");
+        System.out.println("Esto cansado JEfeeeeeeeeeeee");
     }
 }
