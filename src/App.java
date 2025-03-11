c65e8d03797c7be49bb00db23a0dcd782127b9f4
Franco Guarnieri <francoguarnieri90@gmail.com>
2025-03-11 19:09:18 -0300

    Juancito es una masa

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Esto cansado JEfeeeeeeeeeeee");
+        System.out.println("Dale que so vos Juancitooooooooooo");
     }
 }
